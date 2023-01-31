1ff862d5153b1954e20d5b0d0c008c592f90518f
LYS-1 <skyduq123@gmail.com>
2023-01-31 13:58:09 +0900

    01_31_half

--- src/main/java/com/company/home/test/product/ProductService.java
+++ src/main/java/com/company/home/test/product/ProductService.java
@@ -1,9 +1,44 @@
 package com.company.home.test.product;
 
+
+import java.util.List;
+
 import com.company.home.test.product.option.ProductOptionDAO;
 import com.company.home.test.product.option.ProductOptionDTO;
 
+
 public class ProductService {
+	//강한 결합도
+	private ProductOptionDAO productOptionDAO;
+	
+	public ProductService() {
+		
+		this.productOptionDAO = new ProductOptionDAO();
+		
+	}
+	
+	//약한 결합도
+	public void setProductOptionDAO(ProductOptionDAO productOptionDAO) {
+		this.productOptionDAO = productOptionDAO;
+	}
+	
+	public int addProductOption(ProductOptionDTO productOptionDTO, List<ProductOptionDTO> listOption) throws Exception{
+		
+		int productNum = productOptionDAO.getMax();
+		
+		productOptionDTO.setPro_num(productNum);
+		
+		int result = productOptionDAO.addProductOption(productOptionDTO);
+		
+		for(ProductOptionDTO OptionDTO : listOption) {
+			OptionDTO.setPro_num(productNum);
+			result = productOptionDAO.addProductOption(OptionDTO);
+		}
+		
+		return result;
+	}
+	
+	
 	public static void main(String[] args) {
 		ProductDTO productDTO = new ProductDTO();
 		ProductDAO productDAO = new ProductDAO();
